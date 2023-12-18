a24abcc09f1b6e57d37f659a3438373c91fad7f4
evansmwenda <evansmwenda.em@gmail.com>
2023-12-18 19:41:24 +0300

    updated admin assets, removed unneeded files

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -17,45 +17,30 @@
     <!-- End fonts -->
 
     <!-- core:css -->
-    <link rel="stylesheet" href="{{ asset('assets/vendors/core/core.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/core/core.css') }}">
     <!-- endinject -->
 
     <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="{{ asset('assets/vendors/flatpickr/flatpickr.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/flatpickr/flatpickr.min.css') }}">
     <!-- End plugin css for this page -->
 
     <!-- inject:css -->
-    <link rel="stylesheet" href="{{ asset('assets/fonts/feather-font/css/iconfont.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/fonts/feather-font/css/iconfont.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
     <!-- endinject -->
 
     <!-- Layout styles -->  
-    <link rel="stylesheet" href="{{ asset('assets/css/demo1/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/demo1/style.css') }}">
     <!-- End layout styles -->
 
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />
+    <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.png') }}" />
 </head>
 <body>
     <div class="main-wrapper">
 
     <!-- partial:partials/_sidebar.html -->
     @include('admin.partials.sidebar')
-    <nav class="settings-sidebar">
-      <div class="sidebar-body">
-        <a href="#" class="settings-sidebar-toggler">
-          <i data-feather="settings"></i>
-        </a>
-        <div class="theme-wrapper">
-          <h6 class="text-muted mb-2">Light Theme:</h6>
-          <a class="theme-item" href="../demo1/dashboard.html">
-            <img src="../assets/images/screenshots/light.jpg" alt="light theme">
-          </a>
-          <h6 class="text-muted mb-2">Dark Theme:</h6>
-          <a class="theme-item active" href="../demo2/dashboard.html">
-            <img src="../assets/images/screenshots/dark.jpg" alt="light theme">
-          </a>
-        </div>
-      </div>
+
     </nav>
         <!-- partial -->
     
@@ -75,21 +60,21 @@
     </div>
 
     <!-- core:js -->
-    <script src="{{ asset('assets/vendors/core/core.js') }} "></script>
+    <script src="{{ asset('backend/assets/vendors/core/core.js') }} "></script>
     <!-- endinject -->
 
     <!-- Plugin js for this page -->
-  <script src="{{ asset('assets/vendors/flatpickr/flatpickr.min.js') }}"></script>
-  <script src="{{ asset('assets/vendors/apexcharts/apexcharts.min.js') }} "></script>
+  <script src="{{ asset('backend/assets/vendors/flatpickr/flatpickr.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/vendors/apexcharts/apexcharts.min.js') }} "></script>
     <!-- End plugin js for this page -->
 
     <!-- inject:js -->
-    <script src="{{ asset('assets/vendors/feather-icons/feather.min.js') }}"></script>
-    <script src="{{ asset('assets/js/template.js') }} "></script>
+    <script src="{{ asset('backend/assets/vendors/feather-icons/feather.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/template.js') }} "></script>
     <!-- endinject -->
 
     <!-- Custom js for this page -->
-  <script src="{{ asset('assets/js/dashboard-light.js') }}"></script>
+  <script src="{{ asset('backend/assets/js/dashboard-light.js') }}"></script>
     <!-- End custom js for this page -->
 
 </body>
